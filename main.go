def6e0080be6b489d26a3a9a339787707834fd9e
Noel Cower <ncower@gmail.com>
2019-04-09 08:51:02 -0700

    Add additional time formats, support for durations
    - Adds non-standard timestamps that resemble RFC3339 but are distinct
      (space instead of T, no colon in time offset).
    
    - Add space-separated times. These should be uncommon.
    
    - Add durations from current time.
    
    - Add time in seconds as a float to output.
    
    - Add target time in UTC and local time to output.
    
    - Round second and minute times instead of truncating them through
      integer division. This will result in slightly different but
      closer-to-correct output.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
@@ -16,8 +17,12 @@ import (
 )
 
 var timeFormats = []string{
-	"2006-01-02T15:04:05Z07:00",
-	"2006-01-02T15:04:05.999999999Z07:00",
+	"2006-01-02T15:04:05Z0700",
+	"2006-01-02T15:04:05.999999999Z0700",
+	"2006-01-02 15:04:05Z0700",
+	"2006-01-02 15:04:05.999999999Z0700",
+	"2006-01-02 15:04:05Z07:00",
+	"2006-01-02 15:04:05.999999999Z07:00",
 	time.RFC3339,
 	time.RFC3339Nano,
 	time.ANSIC,
@@ -36,6 +41,8 @@ var timeFormats = []string{
 	"3:04:05.999999999pm",
 	"3PM",
 	"3pm",
+	"3 PM",
+	"3 pm",
 	"15",
 	"15:04",
 	"15:04:05",
@@ -56,34 +63,51 @@ func main() {
 
 	now := time.Now()
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s <time>...\n", prog)
+		fmt.Fprintf(os.Stderr, "Usage: %s <time|duration>...\n", prog)
 	}
 	flag.Parse()
 
+	var buf bytes.Buffer
 	for _, ts := range flag.Args() {
-		if t, err := parseTime(ts); err != nil {
+		t, err := parseTime(ts, now)
+		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
-		} else {
-			t = t.UTC()
-			if t.Year() == 0 && t.Day() == 1 && t.Month() == 1 {
-				t = time.Date(now.Year(), now.Month(), now.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), now.Location())
-				if t.Before(now) {
-					t = t.AddDate(0, 0, 1)
-				}
-			}
-			d := t.Sub(now)
-			fmt.Printf("%d\t%d\t%d\t%v\t%d\t%d\n", int64(d/time.Minute), int64(d/time.Second), int64(d), d, t.Unix(), t.UnixNano())
+			continue
 		}
+		t = t.UTC()
+		if t.Year() == 0 && t.Day() == 1 && t.Month() == 1 {
+			t = time.Date(now.Year(), now.Month(), now.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), now.Location())
+			if t.Before(now) {
+				t = t.AddDate(0, 0, 1)
+			}
+		}
+
+		d := t.Sub(now)
+		fmt.Fprintf(&buf, "%d\t%d\t%d\t%v\t%d\t%d\t%f\t%v\t%v\n",
+			int64(d.Round(time.Minute)/time.Minute), // Minutes - rounded
+			int64(d.Round(time.Second)/time.Second), // Seconds - rounded
+			int64(d),                           // Nanoseconds
+			d,                                  // Duration
+			t.Unix(),                           // Seconds since Unix epoch
+			t.UnixNano(),                       // Nanoseconds since Unix epoch (may not make sense past 2262, so shouldn't be a concern to anyone)
+			d.Seconds(),                        // Seconds, floating point (milliseconds and lower units are fractional)
+			t.Format(time.RFC3339Nano),         // RFC3339 time, UTC
+			t.Local().Format(time.RFC3339Nano), // RFC3339 time, local
+		)
 	}
+	buf.WriteTo(os.Stdout)
 }
 
-func parseTime(ts string) (time.Time, error) {
+func parseTime(ts string, now time.Time) (time.Time, error) {
 	for _, tf := range timeFormats {
 		t, err := time.Parse(tf, ts)
 		if err != nil {
 			continue
 		}
 		return t, nil
+	}
+	if d, err := time.ParseDuration(ts); err == nil {
+		return now.Add(d), nil
 	}
 	return time.Time{}, fmt.Errorf("cannot parse time %q", ts)
 }
